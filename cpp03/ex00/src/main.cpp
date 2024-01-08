6b85811a7b2ad763b0b151caccbe7f230f8ced1c
naterrie <naterrie@student.42lyon.fr>
2024-01-08 15:48:19 +0100

    edit main cpp03 ex00

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -5,19 +5,20 @@ int main(void)
 	ClapTrap bob("Bob");
 	ClapTrap marley("marley");
 
+	std::cout << std::endl;
 	bob.attack("marley");
 	marley.takeDamage(bob.get_damage());
 	marley.beRepaired(5);
 	marley.attack("bob");
 	bob.takeDamage(marley.get_damage());
 	bob.beRepaired(5);
-	for (int i = 0; i < 9; i++)
-	{
-		bob.attack("marley");
-		marley.takeDamage(1);
-	}
 	bob.attack("marley");
-	marley.takeDamage(1);
+	marley.takeDamage(10);
+	std::cout << std::endl;
+	for (int i = 0; i < 9; i++)
+		bob.attack("marley");
+	std::cout << std::endl;
 	marley.beRepaired(1);
+	std::cout << std::endl;
 	return 0;
 }
